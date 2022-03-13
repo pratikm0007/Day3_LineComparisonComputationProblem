9c8ef081d4731fa2650a6dbf0bdbe5df1da85b0a
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-13 11:34:29 +0530

    Push UC2 file

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        public static double lengthOfLine1;
+        public static double lengthOfLine2;
+        /// <summary>
+        /// Check equality of two line.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Co-ordinates value for the Length of Lines");
+
+            //Calculation for line1 length
+            Console.WriteLine("Enter value for 1st Line point1 isX1 ");
+            double isX1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value for 1st Line point1 isY1 ");
+            double isY1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value for 1st Line point2 isX2 ");
+            double isX2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value for 1st Line point2 isY2 ");
+            double isY2 = Convert.ToDouble(Console.ReadLine());
+
+            lengthOfLine1 = Math.Sqrt((Math.Pow((isX2 - isX1), 2)) + (Math.Pow((isY2 - isY1), 2)));
+            Console.WriteLine($"Length of line1 is : {lengthOfLine1}");
+
+            //Calculation for line2 length
+            Console.WriteLine("Enter value for 2nd Line point1 isX11 ");
+            double isX11 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value for 2nd Line point1 isY11 ");
+            double isY11 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value for 2nd Line point2 isX22 ");
+            double isX22 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value for 2nd Line point2 isY22 ");
+            double isY22 = Convert.ToDouble(Console.ReadLine());
+
+            lengthOfLine2 = Math.Sqrt((Math.Pow((isX22 - isX11), 2)) + (Math.Pow((isY22 - isY11), 2)));
+            Console.WriteLine($"Length of line2 is : {lengthOfLine2}");
+
+            //To check two line are equal or not.
+            Console.WriteLine("Result Of Equality Check");
+            Console.WriteLine(lengthOfLine1.Equals(lengthOfLine2));
+
+        }
+    }
+}
